531ed4538695d0831e83b44f563e2ce1d4f068ce
AndreMozsi1 <r0930141@student.thomasmore.be>
2023-03-22 12:54:22 +0100

    oef1

--- oef1.py
+++ oef1.py
@@ -1,40 +1,13 @@
-import wiringpi as wp
+import wiringpi
+import time
 
-# set up pins
-IN1 = 0
-IN2 = 1
-IN3 = 2
-IN4 = 3
+LED_PIN = 2
 
-# set pin modes
 wiringpi.wiringPiSetup()
-wiringpi.pinMode(IN1, wiringpi.OUTPUT)
-wiringpi.pinMode(IN2, wiringpi.OUTPUT)
-wiringpi.pinMode(IN3, wiringpi.OUTPUT)
-wiringpi.pinMode(IN4, wiringpi.OUTPUT)
+wiringpi.pinMode(LED_PIN, wiringpi.GPIO.OUTPUT)
 
-# define step sequence
-step_sequence = [
-    [1, 0, 0, 1],
-    [1, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 1, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 1],
-    [0, 0, 0, 1]
-]
-
-# set initial values
-step_count = len(step_sequence)
-current_step = 0
-
-# rotate motor
 while True:
-    for pin in range(4):
-        value = step_sequence[current_step][pin]
-        wiringpi.digitalWrite(IN1+pin, value)
-    current_step += 1
-    if current_step == step_count:
-        current_step = 0
-    time.sleep(0.01)
+    wiringpi.digitalWrite(LED_PIN, wiringpi.GPIO.HIGH) # Turn LED on
+    time.sleep(0.5) # Wait for 0.5 seconds
+    wiringpi.digitalWrite(LED_PIN, wiringpi.GPIO.LOW) # Turn LED off
+    time.sleep(0.5) # Wait for 0.5 seconds
